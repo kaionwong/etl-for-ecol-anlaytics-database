edf4701847beb70b4fee9524262a7a560c7c81ef
kaionwong <kaion@ualberta.ca>
2025-02-21 11:56:55 -0700

    oragnize and place unused code into code_dump folder

---
+++ code_dump/extract_ecollision_oracle_with_upload_pending_or_uploaded_on_cutoff_date_2025-01-16_vOldButGood.sql
@@ -0,0 +1,141 @@
+-- Save the output of this .sql to "extract_ecollision_oracle_with_upload_pending_or_uploaded_on_cutoff_date_YYYY-MM-DD.csv" in the "ecollision-analytics-assessment" directory
+
+WITH CollisionCutoffDates AS (
+    -- collision cut-off date is used to set a maximum time boundary for a case to be considered "valid" when they have "upload pending/uploaded" status; reference = https://ecollisionanalytics-pappa1:14501/eCollisionAnalytics_prd/app/administration/EditingCutoffDatesList.seam?cid=171&conversationPropagation=end
+    -- This CTE defines cutoff dates for each year, indicating the last date a case can be considered valid based on status
+    SELECT 2025 AS created_year, TO_DATE('2026-06-30', 'YYYY-MM-DD') AS cutoff_end_date FROM DUAL UNION ALL
+    SELECT 2024 AS created_year, TO_DATE('2026-06-30', 'YYYY-MM-DD') AS cutoff_end_date FROM DUAL UNION ALL
+    SELECT 2023 AS created_year, TO_DATE('2025-06-30', 'YYYY-MM-DD') AS cutoff_end_date FROM DUAL UNION ALL
+    SELECT 2022 AS created_year, TO_DATE('2024-06-30', 'YYYY-MM-DD') AS cutoff_end_date FROM DUAL UNION ALL
+    SELECT 2021 AS created_year, TO_DATE('2023-02-06', 'YYYY-MM-DD') AS cutoff_end_date FROM DUAL UNION ALL
+    SELECT 2020 AS created_year, TO_DATE('2022-06-15', 'YYYY-MM-DD') AS cutoff_end_date FROM DUAL UNION ALL
+    SELECT 2019 AS created_year, TO_DATE('2021-10-23', 'YYYY-MM-DD') AS cutoff_end_date FROM DUAL UNION ALL
+    SELECT 2018 AS created_year, TO_DATE('2020-01-23', 'YYYY-MM-DD') AS cutoff_end_date FROM DUAL UNION ALL
+    SELECT 2017 AS created_year, TO_DATE('2019-02-11', 'YYYY-MM-DD') AS cutoff_end_date FROM DUAL UNION ALL
+    SELECT 2016 AS created_year, TO_DATE('2018-01-26', 'YYYY-MM-DD') AS cutoff_end_date FROM DUAL UNION ALL
+    SELECT 2015 AS created_year, TO_DATE('2016-01-02', 'YYYY-MM-DD') AS cutoff_end_date FROM DUAL UNION ALL
+    SELECT 2014 AS created_year, TO_DATE('2015-01-02', 'YYYY-MM-DD') AS cutoff_end_date FROM DUAL UNION ALL
+    SELECT 2013 AS created_year, TO_DATE('2014-01-02', 'YYYY-MM-DD') AS cutoff_end_date FROM DUAL UNION ALL
+    SELECT 2012 AS created_year, TO_DATE('2013-01-02', 'YYYY-MM-DD') AS cutoff_end_date FROM DUAL UNION ALL
+    SELECT 2011 AS created_year, TO_DATE('2012-01-02', 'YYYY-MM-DD') AS cutoff_end_date FROM DUAL UNION ALL
+    SELECT 2010 AS created_year, TO_DATE('2011-01-02', 'YYYY-MM-DD') AS cutoff_end_date FROM DUAL UNION ALL
+    SELECT 2009 AS created_year, TO_DATE('2010-01-02', 'YYYY-MM-DD') AS cutoff_end_date FROM DUAL UNION ALL
+    SELECT 2008 AS created_year, TO_DATE('2009-01-02', 'YYYY-MM-DD') AS cutoff_end_date FROM DUAL UNION ALL
+    SELECT 2007 AS created_year, TO_DATE('2008-01-02', 'YYYY-MM-DD') AS cutoff_end_date FROM DUAL UNION ALL
+    SELECT 2006 AS created_year, TO_DATE('2007-01-02', 'YYYY-MM-DD') AS cutoff_end_date FROM DUAL UNION ALL
+    SELECT 2005 AS created_year, TO_DATE('2006-01-02', 'YYYY-MM-DD') AS cutoff_end_date FROM DUAL UNION ALL
+    SELECT 2004 AS created_year, TO_DATE('2005-01-02', 'YYYY-MM-DD') AS cutoff_end_date FROM DUAL
+),
+CollisionEarliestDate AS (
+  -- pulling the earliest collision status history date. This earliest date will be mapped with the corresponding cut-off date, which gives each collision a preset cut-off date
+  -- This CTE retrieves the earliest status change date for each collision from the status history
+  SELECT
+    collision_id,
+    TO_CHAR(MIN(created_timestamp), 'YYYY-MM-DD') AS earliest_created_date
+  FROM
+    ecrdba.cl_status_history
+  GROUP BY
+    collision_id
+),
+CollisionCaseYear AS (
+  -- This CTE extracts the created year from the earliest collision date, establishing the year for the collision case
+  SELECT
+    ced.collision_id,
+    EXTRACT(
+      YEAR
+      FROM
+        TO_DATE(ced.earliest_created_date, 'YYYY-MM-DD')
+    ) AS created_year,
+    ced.earliest_created_date
+  FROM
+    CollisionEarliestDate ced
+),
+CollisionWithCutoff AS (
+  -- This CTE joins the collisions with their corresponding cutoff dates based on the created year
+  SELECT
+    ccy.collision_id,
+    ccy.created_year,
+    ccd.cutoff_end_date
+  FROM
+    CollisionCaseYear ccy
+    JOIN CollisionCutoffDates ccd ON ccy.created_year = ccd.created_year
+),
+CollisionStatusOnCutoff AS (
+  -- This CTE retrieves the status history for collisions that occurred before or on the cutoff date, including ranking them by the effective date
+  SELECT
+    cwc.collision_id,
+    cwc.created_year,
+    cwc.cutoff_end_date,
+    csh.COLL_STATUS_TYPE_ID,
+    TO_CHAR(TO_DATE(csh.EFFECTIVE_DATE, 'YYYY-MM-DD'), 'YY-MM-DD') AS EFFECTIVE_DATE,
+    ROW_NUMBER() OVER (
+      PARTITION BY cwc.collision_id
+      ORDER BY
+        csh.EFFECTIVE_DATE DESC,
+        csh.COLL_STATUS_TYPE_ID DESC
+    ) AS rn
+  FROM
+    CollisionWithCutoff cwc
+    JOIN ecrdba.cl_status_history csh ON cwc.collision_id = csh.collision_id
+    AND TO_DATE(csh.EFFECTIVE_DATE, 'YYYY-MM-DD') <= cwc.cutoff_end_date
+  WHERE
+    TO_DATE(csh.EFFECTIVE_DATE, 'YYYY-MM-DD') <= cwc.cutoff_end_date
+),
+CollisionStatusOnCutoffFiltered AS (
+  -- This CTE filters the collision status records to include only those that are valid at the cutoff date
+  SELECT
+    *
+  FROM
+    CollisionStatusOnCutoff
+  WHERE
+    TO_DATE(effective_date, 'YY-MM-DD') <= cutoff_end_date
+),
+CollisionStatusOnCutoffFilteredTwice AS (
+  -- This CTE further processes the filtered statuses to rank them again and prepare for final selection
+  SELECT
+    collision_id,
+    created_year,
+    cutoff_end_date,
+    coll_status_type_id,
+    effective_date,
+    rn,
+    ROW_NUMBER() OVER (
+      PARTITION BY collision_id
+      ORDER BY
+        rn ASC
+    ) AS rn2
+  FROM
+    CollisionStatusOnCutoffFiltered
+),
+CollisionStatusOnCutoffFilteredThrice AS (
+  -- This CTE selects the top-ranked status for each collision, ensuring that only the most recent valid status is considered
+  SELECT
+    *
+  FROM
+    CollisionStatusOnCutoffFilteredTwice
+  WHERE
+    rn2 = 1
+)
+SELECT
+  csoc.collision_id,
+  csoc.created_year,
+  EXTRACT(YEAR FROM TO_DATE(TO_CHAR(c.OCCURENCE_TIMESTAMP, 'YY-MM-DD'), 'YY-MM-DD')) AS case_year,
+  csoc.cutoff_end_date,
+  csoc.COLL_STATUS_TYPE_ID,
+  csoc.EFFECTIVE_DATE,
+  c.case_nbr,
+  c.PFN_FILE_NBR,
+  TO_CHAR(TO_DATE(c.occurence_timestamp, 'YYYY-MM-DD'), 'YY-MM-DD') AS occurence_timestamp,
+  TO_CHAR(TO_DATE(c.reported_timestamp, 'YYYY-MM-DD'), 'YY-MM-DD') AS reported_timestamp,
+  CASE
+    WHEN csoc.COLL_STATUS_TYPE_ID = 220 THEN 1 -- 220 as upload pending
+    WHEN csoc.COLL_STATUS_TYPE_ID = 221 THEN 1 -- 221 as uploaded
+    ELSE 0
+  END AS valid_at_cutoff_flag
+FROM
+  CollisionStatusOnCutoffFilteredThrice csoc
+  LEFT JOIN ecrdba.collisions c ON csoc.collision_id = c.id
+WHERE
+  1 = 1
+ORDER BY
+  csoc.collision_id;
